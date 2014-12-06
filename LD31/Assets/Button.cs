016d82ead8e1c3f4f49624116dfdad9b64abd27b
paul pettipas <paul.pettipas@gmail.com>
2014-12-06 01:31:59 -0400

    initial commit

---
+++ LD31/Assets/Button.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Button : MonoBehaviour {
+
+	public KeyCode code;
+
+	public void Update(){
+		name = code.ToString();
+	}
+
+	public void KeyDown(){
+		transform.position+=new Vector3(0,1,0);
+	}
+
+	public void KeyUp(){
+		transform.position+=new Vector3(0,-1,0);
+	}
+}
---
+++ LD31/Assets/Ctrl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+//big money control. winitron 1997
+public class Ctrl : MonoBehaviour {
+
+	public Dictionary<KeyCode,Button> buttons = new Dictionary<KeyCode, Button>();
+
+	public void Awake(){
+		foreach(Transform t in transform){
+			Button b = t.GetComponent<Button>();
+			if(b != null){
+				buttons.Add(b.code,b);
+			}
+		}
+	}
+
+	void Update () {
+		foreach (var code in Enum.GetValues(typeof(KeyCode))) {
+			if(Input.GetKeyDown((KeyCode)code)) {
+				Debug.Log (code);
+				if(buttons.ContainsKey((KeyCode)code)){
+					buttons[(KeyCode)code].KeyDown();
+				}
+			}
+			if(Input.GetKeyUp((KeyCode)code)) {
+				if(buttons.ContainsKey((KeyCode)code)){
+					buttons[(KeyCode)code].KeyUp();
+				}
+			}
+		}
+	}
+}
